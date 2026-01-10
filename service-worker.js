cd702253c6c673fd9ecfc0f63c91bd342b53b3a3
Rakshith Ganapur <141945240+rakshith7803@users.noreply.github.com>
2026-01-10 21:58:35 +0530

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,7 @@
-const CACHE_NAME = 'supreme-v1';
-const ASSETS = [
-  'index.html',
-  'style.css',
-  'script.js',
-  'manifest.json',
-  'icons/icon-192.png',
-  'icons/icon-512.png'
-];
-
+const CACHE = 'supreme-v1';
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['index.html', 'style.css', 'script.js'])));
 });
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
