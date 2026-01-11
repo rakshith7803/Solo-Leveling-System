1a5ea954ad593d01679dac4ca1d76c098e5dc856
Rakshith Ganapur <141945240+rakshith7803@users.noreply.github.com>
2026-01-11 20:19:02 +0530

    Update cache name and service worker events

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'supreme-v1';
+const CACHE = 'monarch-v28';
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['index.html', 'style.css', 'script.js'])));
 });
